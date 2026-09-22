239ec91cdf89ef657ad0b318fb8817c1740373c3
Arte8 <artelo.studio@gmail.com>
2026-09-22 19:07:39 -0400

    url var

--- lab2 weather.py
+++ lab2 weather.py
@@ -2,3 +2,5 @@ lab2 weather
 
 import requests
 import pandas as pd
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=43.65&longitude=-79.38&hourly=temperature_2m,precipitation&past_days=7&forecast_days=0&timezone=America/Toronto"
